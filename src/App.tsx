62dbffc555a197ee693582c7641cb85fee121ae6
Hoon Kim <mail@hoonkim.me>
2023-06-15 16:53:43 +0200

    fix format

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
-import ChargingKitListPage from './pages/ChargingKitListPage';
-import TechnicianListPage from './pages/TechnicianListPage';
-import SurveyPage from './pages/SurveyPage';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonSplitPane,
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Redirect, Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import ChargingKitListPage from "./pages/ChargingKitListPage";
+import TechnicianListPage from "./pages/TechnicianListPage";
+import SurveyPage from "./pages/SurveyPage";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
@@ -41,7 +46,7 @@ const App: React.FC = () => {
               <ChargingKitListPage />
             </Route>
             <Route path="/station/:id" exact={true}>
-
+              {/* todo: add the details page */}
             </Route>
 
             {/* todo: need to create all pages */}
